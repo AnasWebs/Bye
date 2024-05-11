2915c724282f26ef7c7efc7d0899a13acc062612
Anas Weird Websites <theanazkhxn@gmail.com>
2024-05-11 15:20:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 setTimeout(function() {
-  window.location.href = 'https://anaswebs.github.io/Hey'; // Redirect to Website 1 after 1 second
+  window.location.href = 'https://anaswebs.github.io/hey'; // Redirect to Website 1 after 1 second
 }, 1000); // 1000 milliseconds = 1 second
